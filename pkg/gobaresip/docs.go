96d4b514ee3c0f200938f721339dd3d5ab1f45e2
f18m <francesco.montorsi@gmail.com>
2025-06-20 16:19:28 +0200

    Fix typo

--- pkg/gobaresip/docs.go
+++ pkg/gobaresip/docs.go
@@ -4,7 +4,7 @@
 // or SIP-based communication systems.
 //
 // For an example of how to use this package, see the example in the
-// `examples` direcotry.
+// `examples` directory.
 //
 // The package provides a [Baresip] struct that represents a Baresip instance and methods
 // to configure it, start it, and interact with it. Looking at the [Baresip] documentation is
